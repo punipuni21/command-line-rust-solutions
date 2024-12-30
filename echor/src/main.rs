3a7aaf55e6cb4d3374217fe4e9875ae5931f776c
punipuni21 <watanaberyotaroyp@gmail.com>
2024-12-30 23:24:55 +0900

    app->command

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,9 @@
+use clap::Command;
+
 fn main() {
-    println!("{:#?}", std::env::args());
+    let _matches = Command::new("echor")
+        .version("0.1.0")
+        .author("ryo")
+        .about("Rust echo")
+        .get_matches();
 }
