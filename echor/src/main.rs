435ef94d66053eccd08ffb6004ff55a408a25816
punipuni21 <watanaberyotaroyp@gmail.com>
2024-12-31 00:09:46 +0900

    other args using Arg::new and num_args

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,9 +1,23 @@
-use clap::Command;
+use clap::{Arg, Command};
 
 fn main() {
     let _matches = Command::new("echor")
         .version("0.1.0")
         .author("ryo")
         .about("Rust echo")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .help("Do not print new line")
+                .num_args(0),
+        )
         .get_matches();
+    println!("{:#?}", _matches);
 }
