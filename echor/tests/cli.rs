5cfbb8d416fd3420154956ec3467f4764eb7f456
punipuni21 <watanaberyotaroyp@gmail.com>
2025-01-01 15:40:19 +0900

    create an helper func

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -39,3 +39,13 @@ fn hello2() -> TestResult {
 
     Ok(())
 }
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
