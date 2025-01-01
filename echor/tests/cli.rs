68a3e6ad120e812bed6aab67db74916644e1f25a
punipuni21 <watanaberyotaroyp@gmail.com>
2025-01-01 15:34:29 +0900

    create testResult

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,25 +1,43 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
-use std::fs;
+use std::{fs, io::Take};
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.assert()
+fn dies_no_args() -> TestResult {
+    Command::cargo_bin("echor")?
+        .assert()
         .failure()
         .stderr(predicates::str::contains("Usage"));
+    Ok(())
 }
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello").assert().success();
+fn runs() -> TestResult {
+    Command::cargo_bin("echor")?.arg("hello").assert().success();
+    Ok(())
 }
 
 #[test]
-fn hello1() {
+fn hello1() -> TestResult {
     let outfile = "tests/expected/hello1.txt";
     let expected = fs::read_to_string(outfile).unwrap();
     let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.arg("Hello there").assert().success().stdout(expected);
+
+    Ok(())
+}
+
+#[test]
+fn hello2() -> TestResult {
+    let outfile = "tests/expected/hello2.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.args(vec!["Hello", "there"])
+        .assert()
+        .success()
+        .stdout(expected);
+
+    Ok(())
 }
