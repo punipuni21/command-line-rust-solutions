c7c11d825f72d1065be0f06aa1122fb22ba0ff81
punipuni21 <watanaberyotaroyp@gmail.com>
2025-01-03 14:07:56 +0900

    add args

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::Ok;
 use clap::{Arg, Command};
 use std::error::Error;
 
@@ -8,4 +9,47 @@ pub struct Config {
     files: Vec<String>,
     lines: usize,
     bytes: Option<usize>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("headr")
+        .version("0.1.0")
+        .about("head")
+        .author("ryo")
+        .arg(
+            Arg::new("lines")
+                .short('n')
+                .long("lines")
+                .value_name("LINES")
+                .help("Input lines")
+                .default_value("10")
+                .num_args(0),
+        )
+        .arg(
+            Arg::new("bytes")
+                .short('c')
+                .long("bytes")
+                .help("Input bytes")
+                .num_args(0),
+        )
+        .arg(
+            Arg::new("file")
+                .value_name("FILE")
+                .help("Input files")
+                .default_value("-")
+                .num_args(1..),
+        )
+        .get_matches();
+
+    let text: Vec<String> = matches
+        .get_many::<String>("lines")
+        .unwrap()
+        .map(|s| s.to_string())
+        .collect();
+
+    Ok(Config {
+        files: text,
+        lines: 10,
+        bytes: None,
+    })
 }
