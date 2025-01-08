c918818c0f8470e61cd57dd6179c843a4bcd829c
punipuni21 <watanaberyotaroyp@gmail.com>
2025-01-08 22:52:18 +0900

    createb config

---
+++ uniqr/src/lib.rs
@@ -0,0 +1,10 @@
+use clap::{Arg, Command};
+use std::error::Error;
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    in_file: String,
+    out_file: Option<String>,
+    count: bool,
+}
