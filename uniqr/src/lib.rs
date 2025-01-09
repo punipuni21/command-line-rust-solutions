2f559fd53665d89383fa9da58084d503b1cd9011
punipuni21 <watanaberyotaroyp@gmail.com>
2025-01-09 23:07:30 +0900

    process file

--- uniqr/src/lib.rs
+++ uniqr/src/lib.rs
@@ -1,5 +1,8 @@
 use clap::{Arg, Command};
-use std::error::Error;
+use std::{
+    error::Error,
+    io::{self, BufRead, BufReader},
+};
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug)]
@@ -52,6 +55,22 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    let mut file = open(&config.in_file).map_err(|e| format!("{}: {}", config.in_file, e))?;
+    let mut line = String::new();
+    loop {
+        let bytes = file.read_line(&mut line)?;
+        if bytes == 0 {
+            break;
+        }
+        print!("{}", line);
+        line.clear();
+    }
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(std::io::BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(std::fs::File::open(filename)?))),
+    }
 }
