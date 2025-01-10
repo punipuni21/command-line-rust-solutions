609643ac1d0e17687c43b88e760387cc4f0738a5
punipuni21 <watanaberyotaroyp@gmail.com>
2025-01-10 23:19:30 +0900

    create lib

---
+++ findr/src/lib.rs
@@ -0,0 +1,20 @@
+use crate::EntryType::*;
+use clap::{App, Arg};
+use regex::Regex;
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn std::error::Error>>;
+
+#[derive(Debug, Eq, PartialEq)]
+enum EntryType {
+    Dir,
+    File,
+    Link,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    paths: Vec<String>,
+    names: Vec<Regex>,
+    entry_type: EntryType,
+}
