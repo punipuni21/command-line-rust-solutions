1f65ea140d36bb14603a5034c242eda8da6d3684
punipuni21 <watanaberyotaroyp@gmail.com>
2025-01-12 14:55:55 +0900

    tmp lib.rs

--- cutr/src/lib.rs
+++ cutr/src/lib.rs
@@ -1,1 +1,43 @@
+use crate::Extract::*;
+use clap::{builder::Str, Arg, Command};
+use std::{error::Error, ops::Range};
 
+type MyResult<T> = Result<T, Box<dyn Error>>;
+type PositionList = Vec<Range<usize>>;
+
+#[derive(Debug)]
+pub enum Extract {
+    Fields(PositionList),
+    Bytes(PositionList),
+    Chars(PositionList),
+}
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    delimiter: u8,
+    extract: Extract,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("curt")
+        .version("0.1.0")
+        .author("ryo")
+        .about("Rust cut")
+        .arg(
+            Arg::new("bytes")
+                .value_name("BYTES")
+                .short('b')
+                .long("bytes")
+                .help("Selected bytes")
+                .num_args(0..),
+        )
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input files")
+                .default_value("-")
+                .num_args(1..),
+        )
+        .get_matches();
+}
