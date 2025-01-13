023207cf8aeed381169def7388ff20a46fc8c5ec
punipuni21 <watanaberyotaroyp@gmail.com>
2025-01-13 22:51:24 +0900

    add unit test

--- cutr/src/lib.rs
+++ cutr/src/lib.rs
@@ -86,6 +86,132 @@ pub fn get_args() -> MyResult<Config> {
     //     })?;
 }
 
-fn parse_pos(range: &str) -> MyResult<PositionList> {
+fn parse_pos(range: String) -> MyResult<PositionList> {
     unimplemented!()
+}
+
+// --------------------------------------------------
+#[cfg(test)]
+mod unit_tests {
+    use super::parse_pos;
+    use csv::StringRecord;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_parse_pos() {
+        // The empty string is an error
+        assert!(parse_pos("".to_string()).is_err());
+
+        // Zero is an error
+        let res = parse_pos("0".to_string());
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "0""#);
+
+        let res = parse_pos("0-1".to_string());
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "0""#);
+
+        // A leading "+" is an error
+        let res = parse_pos("+1".to_string());
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "+1""#,);
+
+        let res = parse_pos("+1-2".to_string());
+        assert!(res.is_err());
+        assert_eq!(
+            res.unwrap_err().to_string(),
+            r#"illegal list value: "+1-2""#,
+        );
+
+        let res = parse_pos("1-+2".to_string());
+        assert!(res.is_err());
+        assert_eq!(
+            res.unwrap_err().to_string(),
+            r#"illegal list value: "1-+2""#,
+        );
+
+        // Any non-number is an error
+        let res = parse_pos("a".to_string());
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "a""#);
+
+        let res = parse_pos("1,a".to_string());
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "a""#);
+
+        let res = parse_pos("1-a".to_string());
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "1-a""#,);
+
+        let res = parse_pos("a-1".to_string());
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), r#"illegal list value: "a-1""#,);
+
+        // Wonky ranges
+        let res = parse_pos("-".to_string());
+        assert!(res.is_err());
+
+        let res = parse_pos(",".to_string());
+        assert!(res.is_err());
+
+        let res = parse_pos("1,".to_string());
+        assert!(res.is_err());
+
+        let res = parse_pos("1-".to_string());
+        assert!(res.is_err());
+
+        let res = parse_pos("1-1-1".to_string());
+        assert!(res.is_err());
+
+        let res = parse_pos("1-1-a".to_string());
+        assert!(res.is_err());
+
+        // First number must be less than second
+        let res = parse_pos("1-1".to_string());
+        assert!(res.is_err());
+        assert_eq!(
+            res.unwrap_err().to_string(),
+            "First number in range (1) must be lower than second number (1)"
+        );
+
+        let res = parse_pos("2-1".to_string());
+        assert!(res.is_err());
+        assert_eq!(
+            res.unwrap_err().to_string(),
+            "First number in range (2) must be lower than second number (1)"
+        );
+
+        // All the following are acceptable
+        let res = parse_pos("1".to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..1]);
+
+        let res = parse_pos("01".to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..1]);
+
+        let res = parse_pos("1,3".to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..1, 2..3]);
+
+        let res = parse_pos("001,0003".to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..1, 2..3]);
+
+        let res = parse_pos("1-3".to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..3]);
+
+        let res = parse_pos("0001-03".to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..3]);
+
+        let res = parse_pos("1,7,3-5".to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..1, 6..7, 2..5]);
+
+        let res = parse_pos("15,19-20".to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![14..15, 18..20]);
+    }
 }
