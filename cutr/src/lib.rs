d8ec02b6fba014c755c509825494bfdbfd56a160
punipuni21 <watanaberyotaroyp@gmail.com>
2025-01-19 22:17:07 +0900

    implement extract_bytes

--- cutr/src/lib.rs
+++ cutr/src/lib.rs
@@ -53,8 +53,14 @@ fn extract_chars(line: &str, char_pos: &[Range<usize>]) -> String {
         .collect()
 }
 
-fn extract_bytes(line: &str, char_pos: &[Range<usize>]) -> String {
-    unimplemented!()
+fn extract_bytes(line: &str, byte_pos: &[Range<usize>]) -> String {
+    let bytes = line.as_bytes();
+    let selected: Vec<_> = byte_pos
+        .iter()
+        .cloned()
+        .flat_map(|range| range.filter_map(|i| bytes.get(i)).copied())
+        .collect();
+    String::from_utf8_lossy(&selected).into_owned()
 }
 
 fn extract_fields(rec: &csv::StringRecord, field_pos: &[Range<usize>]) -> Vec<String> {
