85ec36593e689198df318701d6698d4b67b012e3
punipuni21 <watanaberyotaroyp@gmail.com>
2025-01-19 22:12:38 +0900

    refactor

--- cutr/src/lib.rs
+++ cutr/src/lib.rs
@@ -45,16 +45,13 @@ fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
 
 fn extract_chars(line: &str, char_pos: &[Range<usize>]) -> String {
     let chars: Vec<_> = line.chars().collect();
-    let mut selected: Vec<char> = vec![];
 
-    for range in char_pos.iter().cloned() {
-        for i in range {
-            if let Some(val) = chars.get(i) {
-                selected.push(*val);
-            }
-        }
-    }
-    selected.iter().collect()
+    char_pos
+        .iter()
+        .cloned()
+        .map(|range| range.filter_map(|i| chars.get(i)))
+        .flatten()
+        .collect()
 }
 
 fn extract_bytes(line: &str, char_pos: &[Range<usize>]) -> String {
