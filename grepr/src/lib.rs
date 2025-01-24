c8af7f28fdfdfdae6b20ec5aae6fc8e743912f1c
punipuni21 <watanaberyotaroyp@gmail.com>
2025-01-24 23:34:23 +0900

    get_args

--- grepr/src/lib.rs
+++ grepr/src/lib.rs
@@ -2,6 +2,8 @@ use clap::{Arg, Command};
 use regex::{Regex, RegexBuilder};
 use std::error::Error;
 
+type MyResult<T> = Result<(), Box<dyn Error>>;
+
 #[derive(Debug)]
 pub struct Config {
     pattern: Regex,
@@ -9,4 +11,25 @@ pub struct Config {
     recursive: bool,
     count: bool,
     invert_match: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("grepr")
+        .version("0.1.0")
+        .author("ryo")
+        .about("Rust grep")
+        .get_matches();
+
+    Ok(Config {
+        pattern,
+        files,
+        recursive,
+        count,
+        invert_match,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
 }
