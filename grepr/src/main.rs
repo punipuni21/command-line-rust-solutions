67dc55a7d9dda5df555cbc3c39b273fb8f62d310
punipuni21 <watanaberyotaroyp@gmail.com>
2025-01-24 23:31:12 +0900

    preparation

--- grepr/src/main.rs
+++ grepr/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = grepr::get_args().and_then(grepr::run) {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
 }
