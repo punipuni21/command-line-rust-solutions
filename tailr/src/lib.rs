948ea656eb1aead140bc21a4639f7ea45c57ec08
punipuni21 <watanaberyotaroyp@gmail.com>
2025-02-05 22:45:42 +0900

    generic + trait border

--- tailr/src/lib.rs
+++ tailr/src/lib.rs
@@ -117,11 +117,10 @@ fn get_start_index(take_value: &TakeValue, total_lines: i64) -> Option<u64> {
     unimplemented!()
 }
 
-fn print_bytes<T: Read + Seek>(
-    mut file: T,
-    num_bytes: &TakeValue,
-    total_bytes: i64,
-) -> MyResult<()> {
+fn print_bytes<T>(mut file: T, num_bytes: &TakeValue, total_bytes: i64) -> MyResult<()>
+where
+    T: Read + Seek,
+{
     unimplemented!()
 }
 
