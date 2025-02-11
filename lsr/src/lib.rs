4e05249cb041b0002cae88c06e6e969ac53980bd
punipuni21 <watanaberyotaroyp@gmail.com>
2025-02-11 22:49:57 +0900

    prepare get_args and run func

--- lsr/src/lib.rs
+++ lsr/src/lib.rs
@@ -9,3 +9,22 @@ pub struct Config {
     long: bool,
     show_hidden: bool,
 }
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("lsr")
+        .version("0.1.0")
+        .author("ryo")
+        .about("Rust ls")
+        .get_matches();
+
+    Ok(Config {
+        paths: vec![".".to_string()],
+        long: true,
+        show_hidden: true,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:?}", config);
+    Ok(())
+}
