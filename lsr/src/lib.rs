f8a92f47ca96dd9886e71ea34203fee34a6639a5
punipuni21 <watanaberyotaroyp@gmail.com>
2025-02-11 22:57:40 +0900

    implement get_args

--- lsr/src/lib.rs
+++ lsr/src/lib.rs
@@ -15,12 +15,41 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("ryo")
         .about("Rust ls")
+        .arg(
+            Arg::new("paths")
+                .value_name("PATH")
+                .help("Files and/or directories")
+                .num_args(1..)
+                .default_value("."),
+        )
+        .arg(
+            Arg::new("long")
+                .short('l')
+                .long("long")
+                .help("Long listing")
+                .default_value("false")
+                .num_args(0),
+        )
+        .arg(
+            Arg::new("all")
+                .short('a')
+                .long("all")
+                .help("Show all files")
+                .default_value("false")
+                .num_args(0),
+        )
         .get_matches();
 
+    let paths = matches
+        .get_many::<String>("paths")
+        .unwrap()
+        .map(String::from)
+        .collect();
+
     Ok(Config {
-        paths: vec![".".to_string()],
-        long: true,
-        show_hidden: true,
+        paths: paths,
+        long: matches.get_flag("long"),
+        show_hidden: matches.get_flag("all"),
     })
 }
 
