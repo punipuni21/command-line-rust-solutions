a268c46014c30c096261f7db1f34d6fc9e50f96a
punipuni21 <watanaberyotaroyp@gmail.com>
2025-02-11 22:47:37 +0900

    preparation

---
+++ lsr/src/lib.rs
@@ -0,0 +1,11 @@
+use clap::{Arg, Command};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    paths: Vec<String>,
+    long: bool,
+    show_hidden: bool,
+}
